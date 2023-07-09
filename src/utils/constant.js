3dd24f795453d7085e2b3f6bf8ec48c94d685657
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2023-07-09 03:45:13 +0100

    api base url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,2 @@
 // export const API_BASE = "http://localhost:5000/api/v1";
-export const API_BASE = "https://appacademy-todoapi.onrender.com/api/v1";
+export const API_BASE = "https://appacademy-api.cyclic.app/api/v1";
